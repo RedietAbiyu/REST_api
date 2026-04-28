70650a5fdaf27d3f2c4ad96e1d829103327a2f43
Redeit Abiyu <r16282472@gmail.com>
2026-04-28 22:36:43 +0300

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="node" />
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME!,
+  },
+});
